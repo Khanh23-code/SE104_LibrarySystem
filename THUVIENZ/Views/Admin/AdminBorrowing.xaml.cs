45cd35f8c884132ac06c989e8e74e8e5e3d1ffb8
binss-AUG <ignore6172@gmail.com>
2026-05-25 21:12:44 +0700

    fix(admin): fix layout cycle crash and remove ghost tab

--- THUVIENZ/Views/Admin/AdminBorrowing.xaml.cs
+++ THUVIENZ/Views/Admin/AdminBorrowing.xaml.cs
@@ -20,18 +20,6 @@ namespace THUVIENZ.Views
             this.DataContext = _viewModel;
             this.Loaded += AdminBorrowing_Loaded;
 
-            dgPendingReaders.ItemsSource = new[]
-            {
-                new { ReaderID = "RD_012", ReaderName = "Khang Nguyễn" },
-                new { ReaderID = "RD_043", ReaderName = "Trần Minh Tâm" },
-                new { ReaderID = "RD_089", ReaderName = "Phạm Hoàng Bách" }
-            };
-
-            dgPendingBooks.ItemsSource = new[]
-            {
-                new { BookTitle = "Clean Code - Robert C. Martin", BookCode = "CS-8891", RequestDate = "25/05/2026 09:30" },
-                new { BookTitle = "Design Patterns", BookCode = "CS-1123", RequestDate = "25/05/2026 10:15" }
-            };
         }
 
         private void AdminBorrowing_Loaded(object sender, RoutedEventArgs e)
